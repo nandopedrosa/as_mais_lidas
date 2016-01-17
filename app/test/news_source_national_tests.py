59bf48e2a46e5ff77e7b587438aae5d4868d9f0c
nandopedrosa <fpedrosa@gmail.com>
2016-01-17 15:39:41 -0200

    Add local news source: PB

--- app/test/news_source_national_tests.py
+++ app/test/news_source_national_tests.py
@@ -111,6 +111,10 @@ class NewsSourceNationalTests(unittest.TestCase):
         news, title = news_source_national.get_most_read('localPA')
         self.assertEqual(len(news), 5)
 
+    def test_local_pb(self):
+        news, title = news_source_national.get_most_read('localPB')
+        self.assertEqual(len(news), 5)
+
     def test_getstate(self):
         state = util.getstate('187.111.96.65')
         self.assertEqual('RJ', state)
--- app/views.py
+++ app/views.py
@@ -60,7 +60,7 @@ def get_state():
     state = util.getstate(ip)
 
     # DEBUG - Change here to simulate different locations. Comment otherwise.
-    state = 'PA'
+    state = 'PB'
 
     if state == 'notfound':
         ns_title = ''
