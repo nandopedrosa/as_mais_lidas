e9c4461f20b6dbb6264d7cb15da2b3307f8f6721
Fernando Pedrosa <fpedrosa@gmail.com>
2016-02-03 11:48:32 -0200

    Add 404 and 500 error pages. TODO: translate error messages

--- app/views.py
+++ app/views.py
@@ -138,3 +138,13 @@ def get_locale():
         return 'pt'
 
 
+# noinspection PyUnusedLocal
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+# noinspection PyUnusedLocal
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
