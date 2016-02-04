bba14c4b796a7f4d89ddcdb63383187147aa23fe
Fernando Pedrosa <fpedrosa@gmail.com>
2016-02-04 17:41:04 -0200

    Add config file with environment variables

---
+++ app/config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf8 -*-
+"""
+config.py: Application configurations
+
+__author__ = "Fernando P. Lopes"
+__email__ = "fpedrosa@gmail.com"
+
+"""
+import os
+
+# Security settings for WTForms
+CSRF_ENABLED = True
+SECRET_KEY = 'breakingalldoors'
+
+# Flask-mail settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = 'asmaislidasapp@gmail.com'
+ADMINS = ['fpedrosa@gmail.com']
+
+# available languages
+LANGUAGES = {
+    'en': 'English',
+    'pt': 'Português'
+}
+
+BABEL_DEFAULT_LOCALE = 'pt_BR'
