9a5bdbb76fd2c1b6e16ccbd3536eee6a965c7350
Fernando Pedrosa <fpedrosa@gmail.com>
2018-06-26 16:35:42 -0300

    Fix Reddit

--- app/news_source_reddit.py
+++ app/news_source_reddit.py
@@ -12,21 +12,20 @@ def __re_any(soup):
     """
     Gets the most read entries of the Ask Reddit/TIL/IAmA  subreddits from the last 24 hours
     :param soup: the BeautifulSoup object
-    :param is_iama: indicates if we want the IAmA page, in which case we ignore the requests
     :return: a list with the most read news from the Ask Reddit page
     """
     news = []
 
-    entries = soup.find('div', id='siteTable').find_all('div', class_='thing')
+    entries = soup.find('div', class_="s13nyulz-0 MjxuI s12rq52u-0 jNBfJm").find_all('h2')
 
     i = 0
 
     for entry in entries:
-        title = entry.find('p', class_='title').a.string
+        title = entry.string
         # Ignore Requests
         if 'AMA Request' in title:
             continue
-        link = entry.find('a', class_='bylink comments may-blank')['href']
+        link = entry.parent['href']
         news.append(dict(title=title, link=link))
         i += 1
         if i == 5:
