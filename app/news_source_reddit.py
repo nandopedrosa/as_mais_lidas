b1eab1632f7db6a9f47600b4e5ce66b45df90370
nandopedrosa <fpedrosa@gmail.com>
2023-02-11 15:03:24 -0300

    Debugging reddit

--- app/news_source_reddit.py
+++ app/news_source_reddit.py
@@ -21,6 +21,7 @@ def __re_any(subreddit):
     posts = []
     base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
     request = requests.get(base_url, headers={'User-agent': 'asmaislidas'})
+    print("REQUEST RESULT: " + str(request))
     json_data = request.json()
 
     for post in json_data['data']['children']:
